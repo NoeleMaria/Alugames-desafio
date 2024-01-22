cba9cdbd2402f02e875dcf6dc07baa443f3a4d0a
Noele Maria <noele.maria.nm@gmail.com>
2024-01-22 12:18:28 -0300

    Adiciona contagem de jogos

--- js/app.js
+++ js/app.js
@@ -1,20 +1,26 @@
+let numeroJogosAlugados = 1; //Já se conta 1, pois já está definido no HTML
+
+function contarJogosAlugadosNoMomento () {
+    console.log(`Total de jogos alugados: ${numeroJogosAlugados}`);
+}
+
 function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
     let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
-   
     
     if (imagem.classList.contains('dashboard__item__img--rented')) {
-
-        if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}?`)) {
-            imagem.classList.remove('dashboard__item__img--rented');
-            botao.classList.remove('dashboard__item__button--return');
-            botao.textContent = 'Alugar';
-        }
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+        numeroJogosAlugados--;
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');  
         botao.textContent = 'Devolver';
+        numeroJogosAlugados++;
     }
+    contarJogosAlugadosNoMomento();
 }
+
